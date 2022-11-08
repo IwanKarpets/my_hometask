b982c8dbe2ef92648133ff19545de160b2299442
IwanKarpets <48625449+IwanKarpets@users.noreply.github.com>
2022-11-08 16:37:53 +0300

    Update rectangle.js
    Update  rectangle.js file

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,14 +1,14 @@
 const checkIsNumber = require('./utils');
 
 function getRectanglePerimeter(length, width) {
-  if (!checkIsNumber(length) || !checkIsNumber(width)) {
+  if (checkIsNumber(length) || checkIsNumber(width)) {
     return console.log(`One or both parameters aren't numbers`);
   }
   return 2 * (length + width);
 }
 
 function getRectangleArea(length, width) {
-  if (!checkIsNumber(length) || !checkIsNumber(width)) {
+  if (checkIsNumber(length) || checkIsNumber(width)) {
     return console.log(`One or both parameters aren't numbers`);
   }
   return length * width;
